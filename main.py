10780f6d4575a5f70cdb2f48f20ec74dc95d1384
Alex Sindalovsky <alex@tinytap.it>
2023-06-07 14:38:02 +0300

    app.get("/tinytap-games/"+category)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 import quart
 import quart_cors
@@ -10,7 +11,14 @@ app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.c
 async def sayHello(name):
     return quart.Response(response=f"Hello {name}!", status=200, mimetype="text/json")
 
-
+@app.get("/tinytap-games/<string:category>")
+async def get_games(category):
+    r = requests.get("https://staging.tinytap.it/store/api/content/search/{category}?language=all&ageGroup=all&include_courses=1&ver=3.5&page_num=1&per_page=20")
+    d = r.json()
+    
+    games = d['data']
+    print(games)
+    return quart.Response(response=json.dumps(games), status=200, mimetype="text/json")
 
 @app.get("/logo.png")
 async def plugin_logo():
